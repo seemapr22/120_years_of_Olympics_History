c6cb45c7556e534800e164f58dcdeb0c3f9fd064
seemapr22 <138873763+seemapr22@users.noreply.github.com>
2023-08-19 11:36:11 +0530

    Update 120 years of Olympics History.sql

--- 120 years of Olympics History.sql
+++ 120 years of Olympics History.sql
@@ -1,15 +1,5 @@
 -- 120 years of Olympics History --
 
-
--- To write and understand SQL queries, first thing is to understand the dataset. 
--- After understanding the dataset, it becomes much simpler to write SQL Queries to retrieve any information from that data.
-
--- Here, I have tried to write SQL Queries on a real dataset. I have downloaded the 120 years of Olympics History dataset from Kaggle. 
--- This way instead of using cooked up data, I have used real data to write my SQL Queries. 
--- By understanding these queries, we can easily retrieve the information from other types of problems also.
-
-
-
 --1. total olympics games that have been held till 2016-- 
 
 SELECT COUNT(DISTINCT games) AS total_olympic_games
